f8a3be638a10463bb0dc22ac1e7f63756417d902
Alvin Cheng <ascheng21@gmail.com>
2017-09-23 12:00:01 -0700

    Set up yelp search API. #1

--- index.js
+++ index.js
@@ -3,11 +3,32 @@ require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
+const yelp = require('yelp-fusion')
+
+const clientId = process.env.CLIENT_ID
+const clientSecret = process.env.CLIENT_SECRET
 
 const app = express()
 
 // app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json())
+
+const searchRequest = {
+  term:'Four Barrel Coffee',
+  location: 'san francisco, ca'
+};
+
+yelp.accessToken(clientId, clientSecret).then(response => {
+  const client = yelp.client(response.jsonBody.access_token);
+
+  client.search(searchRequest).then(response => {
+    const firstResult = response.jsonBody.businesses[0];
+    const prettyJson = JSON.stringify(firstResult, null, 4);
+    console.log(prettyJson);
+  });
+}).catch(e => {
+  console.log(e);
+});
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log('Listening on ' + port))
