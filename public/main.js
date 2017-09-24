096fd0f4dd289927d417b6598818746d772ab44d
Alvin Cheng <ascheng21@gmail.com>
2017-09-23 17:10:25 -0700

    Added geolocation API. #2

--- public/main.js
+++ public/main.js
@@ -2,9 +2,12 @@ const $restaurantButton = document.querySelector('#restaurant-btn')
 const $restaurant = document.querySelector('#restaurant')
 
 $restaurantButton.addEventListener('click', () => {
-  fetch('/restaurant', { method: 'GET' }).then(response => response.json())
-    .then(restaurant => {
-      console.log(restaurant)
-      $restaurant.textContent = restaurant.name
-    })
+  navigator.geolocation.getCurrentPosition(position => {
+    console.log(position.coords.latitude, position.coords.longitude)
+  })
+  // fetch('/restaurant', { method: 'GET' }).then(response => response.json())
+  //   .then(restaurant => {
+  //     console.log(restaurant)
+  //     $restaurant.textContent = restaurant.name
+  //   })
 })
